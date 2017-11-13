27cd239eb980bb00f1e52952eb8d7037add74758
Rasmus Mattas <mara16@nti.umea>
2017-11-13 09:38:03 +0100

    Initial commit

---
+++ src/Spel1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Spel1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nr1 = " ", nr2 = " ", nr3 = " ", nr4 = " ", nr5 = " ", nr6 = " ", nr7 = " ", nr8 = " ", nr9 = " ";
+        int val;
+        System.out.println(nr1 + "|" + nr2 + "|" + nr3 + "\n-----\n" + nr4 + "|" + nr5 + "|" + nr6 + "\n-----\n" + nr7 + "|" + nr8 + "|" + nr9);
+        val = scan.nextInt();
+        if(val == 1){
+            nr1 = "X";
+        }
+
+    }
+}
+
+class board{
+    public static String main(int nr1,int nr2,int nr3){
+
+    }
+}
