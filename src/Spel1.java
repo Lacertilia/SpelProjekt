d6952105a08b4b87000bd35d20d700a097fce11a
Lacertilia <te16.rasmus.mattas@umea.nti.se>
2017-11-13 10:26:41 +0100

    First switch

--- src/Spel1.java
+++ src/Spel1.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Spel1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String nr1 = " ", nr2 = " ", nr3 = " ", nr4 = " ", nr5 = " ", nr6 = " ", nr7 = " ", nr8 = " ", nr9 = " ";
+        char nr1 = ' ', nr2 = ' ', nr3 = ' ', nr4 = ' ', nr5 = ' ', nr6 = ' ', nr7 = ' ', nr8 = ' ', nr9 = ' ';
         int val;
-        System.out.println(nr1 + "|" + nr2 + "|" + nr3 + "\n-----\n" + nr4 + "|" + nr5 + "|" + nr6 + "\n-----\n" + nr7 + "|" + nr8 + "|" + nr9);
+
         val = scan.nextInt();
         if(val == 1){
-            nr1 = "X";
+            nr1 = 'X';
         }
 
     }
 }
 
 class board{
-    public static String main(int nr1,int nr2,int nr3){
-
+    public static void main(int nr1,int nr2,int nr3,int nr4,int nr5,int nr6,int nr7,int nr8,int nr9){
+        System.out.println(nr1 + "|" + nr2 + "|" + nr3 + "\n-----\n" + nr4 + "|" + nr5 + "|" + nr6 + "\n-----\n" + nr7 + "|" + nr8 + "|" + nr9);
     }
 }
